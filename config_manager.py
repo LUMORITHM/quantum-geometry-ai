e57741e06d37833e2acdfbe88942e0730394d9a8
Lumina <Saeed.amiini@gmail.com>
2025-04-04 17:53:11 +0300

    Update config_manager.py

--- config_manager.py
+++ config_manager.py
@@ -1,1 +1,14 @@
+# config_manager.py
+import yaml
 
+class Config:
+    def __init__(self, path="config.yaml"):
+        with open(path, "r") as f:
+            self.config = yaml.safe_load(f)
+
+    def get(self, key_path, default=None):
+        keys = key_path.split(".")
+        val = self.config
+        for key in keys:
+            val = val.get(key, {})
+        return val if val else default
